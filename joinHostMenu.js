5a06e278bbfcb93c8ff5fe236cda9db718fc8a26
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2021-06-28 19:22:36 -0400

    added host/join buttons + click function

---
+++ joinHostMenu.js
@@ -0,0 +1,10 @@
+var joinButton = document.getElementById("joinButton");
+var hostButton = document.getElementById("hostButton");
+
+joinButton.onclick = function () {
+  alert("join");
+};
+
+hostButton.onclick = function () {
+  alert("host");
+};

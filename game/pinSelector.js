ddb70d94eeef220bcfcad29c6287bd39824aefea
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2021-06-29 01:05:31 -0400

    feedback pins work, checking doesn't

--- game/pinSelector.js
+++ game/pinSelector.js
@@ -78,6 +78,9 @@ function mousePressed() {
 }
 
 function displayPins() {
+  //////////////////
+  // Display Pins //
+  //////////////////
   for (let row = 0; row < pins.length; row++) {
     let r = row;
     for (let p = 0; p < pins[r].length; p++) {
@@ -94,23 +97,36 @@ function displayPins() {
       );
     }
   }
+
+  ////////////////////
+  // Display Checks //
+  ////////////////////
+  for (let r = 0; r < currentRow; r++) {
+    let check = checkRow(r);
+    for (let p = 0; p < check[0] + check[1]; p++) {
+      pg.fill(0);
+      if (p < check[1]) {
+        pg.fill(255);
+      }
+      pg.ellipse(
+        size[0] - pinRadius - (pinRadius / 2) * p,
+        150 + (pins.length - 1 - r) * 2 * pinRadius,
+        pinRadius / 4,
+        pinRadius / 4
+      );
+    }
+  }
 }
 function keyPressed() {
   if (key === " ") {
-    checkRow();
+    incIfFull();
   }
 }
-function checkRow() {
-  for (p of pins[currentRow]) {
-    if (p === -1) {
-      alert("hi");
-      return;
-    }
-  }
+function checkRow(r) {
   let guessGraph = new Array(8).fill(0);
   let codeGraph = new Array(8).fill(0);
 
-  for (let c of pins[currentRow]) {
+  for (let c of pins[r]) {
     guessGraph[c]++;
   }
   for (let c of gameSettings.code) {
@@ -119,18 +135,25 @@ function checkRow() {
 
   let softHit = 0;
   let hardHit = 0;
-  for (let c = 0; c < pins[currentRow].length; c++) {
+  for (let c = 0; c < pins[r].length; c++) {
     softHit += Math.min(guessGraph[c], codeGraph[c]);
   }
 
-  for (let c = 0; c < pins[currentRow].length; c++) {
-    if (pins[currentRow][c] === gameSettings.code[c]) {
+  for (let c = 0; c < pins[r].length; c++) {
+    if (pins[r][c] === gameSettings.code[c]) {
       hardHit++;
       softHit--;
     }
   }
-  console.log(guessGraph);
-  console.log(codeGraph);
-  console.log(hardHit, softHit);
+
+  return [hardHit, max(softHit, 0)];
+}
+
+function incIfFull() {
+  for (p of pins[currentRow]) {
+    if (p === -1) {
+      return false;
+    }
+  }
   currentRow++;
 }
